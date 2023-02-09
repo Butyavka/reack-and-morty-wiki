3d677986e9ca45f76a5bb8ca95009010004ce190
Butyavka <oxana199509@gmail.com>
2023-02-09 12:19:05 +0300

    Refactor Characters component

--- src/components/Characters/index.tsx
+++ src/components/Characters/index.tsx
@@ -1,42 +1,42 @@
-import React from 'react'
+import React, { FC } from 'react'
 import './style.scss'
 import CharacterCard, { ICharacterCard } from '../CharacterCard'
 import Loading from '../UI/Loading'
-import { useQuery } from '@apollo/client'
-import { CHARACTERS } from '../../apollo/rickAndMorty'
+import { ApolloError } from '@apollo/client'
 import Error from '../Error'
 import block from '../../helpers/bem'
 
 const b = block('characters-block')
 
-const Characters = () => {
-    const { loading, data, error } = useQuery(CHARACTERS, { variables: { page: 1 }})
-    const characters: ICharacterCard[] = data?.result.characters
+interface ICharacters {
+    characters: ICharacterCard[];
+    loading: boolean;
+    error: ApolloError | null
+}
 
+const Characters: FC<ICharacters> = ({ characters, loading, error }) => {
     if (error) return <Error { ...error }/>
 
-    if (loading) {
-        return <Loading size={ 40 }/>
-    } else {
-        return (
-            <div className={ b('container').mix('container') }>
-                <ul className={ b() }>
-                    {characters.map(character => (
-                        <CharacterCard
-                            key={character.id}
-                            id={character.id}
-                            name={character.name}
-                            image={character.image}
-                            status={character.status}
-                            location={character.location}
-                            origin={character.origin}
-                            species={character.species}
-                        />
-                    ))}
-                </ul>
-            </div>
-        )
-    }
+    if (loading) return <Loading size={ 40 }/>
+
+    return (
+        <div className={ b('container').mix('container') }>
+            <ul className={ b() }>
+                {characters.map(character => (
+                    <CharacterCard
+                        key={character.id}
+                        id={character.id}
+                        name={character.name}
+                        image={character.image}
+                        status={character.status}
+                        location={character.location}
+                        origin={character.origin}
+                        species={character.species}
+                    />
+                ))}
+            </ul>
+        </div>
+    )
 }
 
 export default Characters
--- src/pages/Main/index.tsx
+++ src/pages/Main/index.tsx
@@ -1,10 +1,16 @@
 import React from 'react'
 import Characters from '../../components/Characters'
+import { useQuery } from '@apollo/client'
+import { CHARACTERS } from '../../apollo/rickAndMorty'
+import { ICharacterCard } from '../../components/CharacterCard'
 
 const Main = () => {
+    const { loading, data, error } = useQuery(CHARACTERS, { variables: { page: 1 }})
+    const characters: ICharacterCard[] = data?.result.characters
+
     return (
         <div className="page">
-            <Characters/>
+            <Characters characters={ characters } loading={ loading } error={ error || null }/>
         </div>
     )
 }
