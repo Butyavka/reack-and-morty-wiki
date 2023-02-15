18e05233f4f36b983be27858b58ded98411c5524
Butyavka <oxana199509@gmail.com>
2023-02-15 15:13:23 +0300

    Create Header component

--- src/components/Layout/index.tsx
+++ src/components/Layout/index.tsx
@@ -1,9 +1,11 @@
 import React from 'react'
 import Footer from '../Footer'
+import Header from '../Header'
 
 const Layout = ({ children }: { children: React.ReactNode }) => {
     return (
         <div>
+            <Header/>
             {children}
             <Footer/>
         </div>
